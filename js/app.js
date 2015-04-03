001229b537702740f5054cc67da508d38b48c6f7
Kevin Duell <kdubzot9@gmail.com>
2015-04-03 13:51:24 -0700

    Make changes to open page without errors.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
+; // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
@@ -24,12 +24,24 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite='images/char-boy.png';
+}
+
+Player.prototype.update = function(dt) {
+
+}
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x,this.y);
+}
+
+player = new Player();
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
 
 
 // This listens for key presses and sends the keys to your
