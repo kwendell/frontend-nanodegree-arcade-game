269b1501723492ef214cfa996d937e6496409d83
Kevin Duell <kdubzot9@gmail.com>
2015-04-15 21:27:14 -0700

    Add Rectangle object, prototype.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,36 @@
 // Enemies our player must avoid
+// Model prototype constructor
 
+//canvasWidth=505;
+//canvasHeight=606;
+//  Rectantgle prototype constructor
+var Rectangle = function(x,y,width,height) {
+    console.log("calling rectangle constructor");
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+}
+Rectangle.prototype.setLocation = function(x,y) {
+    this.x=x;
+    this.y=y;
+}
+Rectangle.prototype.containsPoint= function(x,y) {
+    var withinXBounds = (x<=this.x+this.width && x>=this.x) && (y<=this.y+this.height && y>=this.y);
+    return withinXBounds;
+}
+Rectangle.prototype.intersects= function(otherRectangle) {
+    // if any verties of the other rectangle 
+    // are within this rectantle return true
+    var vertices = [[otherRectangle.x,otherRectangle.y],[otherRectangle.x+otherRectangle.width,otherRectangle.y],
+    [otherRectangle.x+otherRectangle.width,otherRectangle.y+otherRectangle.height],[otherRectangle.x,otherRectangle.y+otherRectangle.height]];
+    var retval = false;
+    for (var i=0;i<vertices.length;i++)  {
+        retval = retval || this.containsPoint(vertices[i][0],vertices[i][1]);
+    }
+    return retval;
+
+}
 
 var Enemy = function(timeToTraverse,index) {
 ; // Variables applied to each of our instances go here,
@@ -9,34 +40,37 @@ var Enemy = function(timeToTraverse,index) {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 
+
     // set the initial enemay coordinates to
     // the botoom of the screen.
     this.x = 0;
     this.y = 0;
     this.time=timeToTraverse;
     this.index=index;
-    this.enemyHeight = 171;
+    this.height = 171;
+    this.width = 101;
+    this.rectangle = new Rectangle(this.x,this.y,this.width,this.height);
    
 }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    // dt is the time in seconds since 
-    // the last frame 
+   
   
   var canvasInstance = $("canvas");
  // console.log("time to traverse is"+this.time);
-   var width = canvasInstance[0].width;
+   var width = canvasWidth;//canvasInstance[0].width;
    
    var desiredVelocity  = width/this.time;
    var deltaX = desiredVelocity*dt;
    this.x+=deltaX;
    // wrap the movement when it goes off-screen.
    this.x=this.x % width;
+
+  // console.log(this.rectangle);
+   // update rectantgle
+   //this.rectangle.setLocation(this.rectangle,this.x,this.y);
    //this.x=this.x & width;
 
 
@@ -49,9 +83,6 @@ Enemy.prototype.render = function() {
    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
 
-   // console.log("Enemy::render");
-
-    // var now = Date.now();
    //if (now % 10==0) {console.log("hello");}
 }
 
@@ -66,21 +97,21 @@ var Player = function() {
   // var height = canvasInstance[0].height;
  
    
-    this.imageWidth = 110;
-    this.imageHeight =171;
+    this.width = 110;
+    this.height =171;
 
    
     // k2 todo, use model to access canvas dimensions
-     this.x=505/2-101/2;
-    this.y=606-this.imageHeight;
+    this.x=505/2-101/2;
+    this.y=606-this.height;
 
 
 }
 
 Player.prototype.update = function(dt) {
 
-    var canvasInstance = $("canvas");
    
+
 
 }
 Player.prototype.render = function() {
@@ -93,30 +124,32 @@ Player.prototype.handleInput = function(keyCode) {
        
        //k2 todo use singleton to access tile dimnsions
       
-       this.x-=505/5;
+       this.x-=canvasWidth/5;
        
        if (this.x<0) {
 
-        this.x=505+this.x;
+        this.x=canvasWidth+this.x;
        }
       
 
     } else if (keyCode=="right") {
        
-        this.x+=505/5;
-        if (this.x+101>505) {
+        this.x+=canvasWidth/5;
+        if (this.x+101>canvasWidth) {
 
-        this.x=this.x-505;
+        this.x=this.x-canvasWidth;
        }
 
     } else if (keyCode=="up") {
      
-        this.y-=606/5;
+        this.y-=canvasHeight/5;
 
     } else if (keyCode=="down") {
         
-        this.y+=606/5;
+        this.y+=canvasHeight/5;
     }
+
+    // update the rectantle
 
 }
 
