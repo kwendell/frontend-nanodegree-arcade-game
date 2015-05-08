7b5d90d69c8c4d05f0a094dbb530789342a9ebf4
kevind <kevind@altierre.com>
2015-05-08 09:18:30 -0700

    jslint checkpoint'd up to line 89.  Please enter the commit message for your changes. Lines starting

--- js/app.js
+++ js/app.js
@@ -59,40 +59,39 @@ var Singleton = (function () {
 }());
 
 
-
-
 /* The Rectangle prototype is used to determine
  * collisions by providing an intersects method.
  * Composition is preferred to inheritance, each
  * entity, (enemy, player and reward) has an instance of a Rectangle
  */
-var Rectangle = function(x,y,width,height) {
-    this.x=x;
-    this.y=y;
-    this.width=width;
-    this.height=height;
+ 
+var Rectangle = function (x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
 };
-Rectangle.prototype.setX = function(x) {
-    this.x=x;
+Rectangle.prototype.setX = function (x) {
+    this.x = x;
 };
 
-Rectangle.prototype.setY = function(y) {
-    this.y=y;
+Rectangle.prototype.setY = function (y) {
+    this.y = y;
 };
 
 /*  This is a helper function for the the
  *  intersects() method.
  */
-Rectangle.prototype.containsPoint= function(x,y) {
-    var withinXBounds = x<=this.x+this.width && x>=this.x &&
-                        y<=this.y+this.height && y>=this.y;
+Rectangle.prototype.containsPoint = function (x, y) {
+    var withinXBounds = x <= this.x + this.width && x >= this.x &&
+            y <= this.y + this.height && y >= this.y;
     return withinXBounds;
 };
 /*
  * The insterects method is used to determine collisions.
  */
 
-Rectangle.prototype.intersects= function(otherRectangle) {
+Rectangle.prototype.intersects= function (otherRectangle) {
 
     /* if any vertices of the other rectangles
      * are within this rectantle, return true
